67f1243a0bbf99eef2a26e2fee434d9451e6218c
googlerax <googlerax@hotmail.com>
2023-04-15 23:16:24 -0300

    se realiza varios fixes. ver README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### Grupo 10
 ---
+fix checkbox: ya se seleccionan solo uno
+fix select name="plan: ya se muestra el mensaje del campo select del formulario
+fix se arregla menu de navegacion de cada pagina
+
+
+se agrega link de ubicaciones aleatorias de cada sucursal
+se agrega funcion que enfoca campos invalidos al enviar el formulario
--- landig-page/app.js
+++ landig-page/app.js
@@ -1,119 +1,104 @@
 /* validacion de formulario con jQuery */
 
-
 // evita el envio del form
 jQuery.validator.setDefaults({
-    debug: true,
-    success: "valid"
+  debug: true,
+  success: "valid",
+});
+
+$(document).ready(function () {
+  // metodo validate de jquery
+  $("#contact").validate({
+    //reglas:
+    rules: {
+      nombre: {
+        required: true,
+        minlength: 3,
+      },
+      apellido: {
+        required: true,
+        minlength: 3,
+      },
+      edad: {
+        required: true,
+        number: true,
+        min: 18,
+      },
+
+      email: {
+        required: true,
+        email: true,
+      },
+
+      genero:{
+        required: true,
+
+      },
+
+    //arreglado
+      plan: {
+        required: true,
+      },
+
+      mensaje: {
+        minlength: 30,
+      },
+    },
+    //mensajes personalizados
+    messages: {
+      nombre: {
+        required: `Campo obligatorio`,
+        minlength: `El nombre debe ser mayor a 3 caracteres`,
+      },
+      apellido: {
+        required: `Campo obligatorio`,
+        minlength: `El apellido debe ser mayor a 3 caracteres`,
+      },
+      edad: {
+        required: `Campo obligatorio`,
+        number: `Ingrese un valor numerico`,
+        min: `Debe ser mayor de 18 años`,
+      },
+      email: {
+        required: `Campo obligatorio`,
+        email: `Debe tener un formato abc@domain.tld`,
+      },
+
+      genero:{
+
+        required: ` `
+      },
+      plan: {
+        required: `Seleccione uno`,
+      },
+
+      mensaje: {
+        required: `Campo obligatorio`,
+        minlength: `Debe tener un minimo de 30 caracteres`,
+      },
+    },
   });
+});
 
-$(document).ready(function() {
-     // metodo validate de jquery
-    $("#contact").validate({
-        //reglas:
-        rules:{
-            nombre:{
-                required:true,
-                minlength: 3
-            },
-            apellido:{
-                required:true,
-                minlength: 3
-            },
-            edad:{
-                required: true,
-                number: true,
-                min: 18
-            },
+/* enfoca sobre el campo invalido al enviar el formulario */
 
-            email:{
-                required: true,
-                email:true
-            },
-            //? plan no muestra el mensaje
-            plan:{
-                required:true
-            },
+// cuando se hace clic en el botón de enviar el formulario
+$(".btn btn-danger").click(function () {
+  // si el formulario no es válido
+  if (!$("#contact").valid()) {
+    // busca el primer campo que no haya sido validado
+    var primerCampoInvalido = $("#contact").find(":input.error:first");
+    // enfoca en el primer campo inválido
+    primerCampoInvalido.focus();
+    primerCampoInvalido.val("");
+  }
+});
 
-            mensaje:{
-                required:true,
-                minlength: 30
-            }
-        },
-        //mensajes personalizados
-        messages:{
-            nombre:{
-                required: `Campo obligatorio`,
-                minlength: `El nombre debe ser mayor a 3 caracteres`
-            },
-            apellido:{
-                required: `Campo obligatorio`,
-                minlength: `El apellido debe ser mayor a 3 caracteres`
-            },
-            edad:{
-                required: `Campo obligatorio`,
-                number: `Ingrese un valor numerico`,
-                min: `Debe ser mayor de 18 años`
-            },
-            email:{
-                required: `Campo obligatorio`,
-                email: `Debe tener un formato abc@domain.tld`
-            },
-            plan:{
-                required: `Seleccione uno`
-            },
+/* funcion jquery que permite la seleccion solo de un check */
+$(document).ready(function () {
 
-            mensaje:{
-                required:`Campo obligatorio`,
-                minlength: `Debe tener un minimo de 30 caracteres`
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    });
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(".form-check-input").click(function () {
+    //cuando se selecciona un check ,prop("checked,false") deselecciona los demas
+    $(".form-check-input").not(this).prop("checked", false);
+  });
 });
